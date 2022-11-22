cafba9690e8ff2751c82a13c146f52127ccabb6d
Seif Ammar <sifon@Seifs-MBP.home>
2022-11-22 21:48:35 +0100

    Styling_Templates

--- market.py
+++ market.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+    return render_template('home.html')
+
+
+
+app.debug = True
+app.run(debug=True, use_debugger=False, use_reloader=False)
