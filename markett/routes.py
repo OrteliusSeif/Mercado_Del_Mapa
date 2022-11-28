1fa1a049f83ff5f33a26a0aa379b4a2d60b57920
Seif Ammar <sifon@Seifs-MBP.home>
2022-11-29 00:59:03 +0100

    Flask Validators set

--- markett/routes.py
+++ markett/routes.py
@@ -1,7 +1,9 @@
 from markett import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from markett.models import Item, User
 from markett.forms import RegisterForm
+from markett import db
+
 
 
 
@@ -23,9 +25,21 @@ def market_page():
     #]
     
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_adress= form.email_adress.data, 
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors !={}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
+
+
     return render_template('register.html', form=form)
 
 
